460834d45d6ac59277aacf32c066c6d7218eba81
Danny Lin <danny0838@gmail.com>
2014-11-04 11:13:04 +0800

    Add a php script that reads native dokuwiki conf settings directly. The path argument now takes dokuwiki directory instead of dokuwiki data directory.

---
+++ dokuwiki.php
@@ -0,0 +1,12 @@
+<?php
+if ('cli' != php_sapi_name()) die('This script must run via the command line interface.');
+$dw = @rtrim($argv[1], '/');
+if (!$dw) die("Usage: php dokuwiki.php <dokuwiki directory>\n");
+require_once("$dw/inc/init.php");
+
+$output = array(
+	'conf' => $conf,
+	'config_cascade' => $config_cascade,
+);
+
+echo json_encode($output);
